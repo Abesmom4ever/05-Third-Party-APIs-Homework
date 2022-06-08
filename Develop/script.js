c441af22804f2ee24c0cd5154763baa02aed33f5
Rejean Chung <rejeanpc@gmail.com>
2022-06-07 21:49:08 -0700

    Completed adding present, past and future classes and the local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,144 @@
 const currentDay = $("#currentDay");
 // const hourDisplay = document.getElementById("timeDisplay");
-const textBox = document.querySelectorAll("textarea");
-var saveButton = document.querySelectorAll("button");
+// const textBox = document.querySelectorAll("textarea");
 // const timeDiv = document.getElementById("TimeContainer");
+var saveButton = document.querySelectorAll("button");
+var NineAM = document.getElementById("NineAM");
+var TenAM = document.getElementById("TenAM");
+var ElevenAM = document.getElementById("ElevenAM");
+var TwelvePM = document.getElementById("TwelvePM");
+var OnePM = document.getElementById("OnePM");
+var TwoPM = document.getElementById("TwoPM");
+var ThreePM = document.getElementById("ThreePM");
+var FourPM = document.getElementById("FourPM");
+var FivePM = document.getElementById("FivePM");
 
 function setPlans() {
-    var textValue = textBox.value;
-    localStorage.setItem("Plans", textValue);
+    var NineValue = NineAM.value.trim();
+    var TenValue = TenAM.value.trim();
+    var ElevenValue = ElevenAM.value.trim();
+    var TwelveValue = TwelvePM.value.trim();
+    var OneValue = OnePM.value.trim();
+    var TwoValue = TwoPM.value.trim();
+    var ThreeValue = ThreePM.value.trim();
+    var FourValue = FourPM.value.trim();
+    var FiveValue = FivePM.value.trim();
+    localStorage.setItem("NineAMPlans", NineValue);
+    localStorage.setItem("TenAMPlans", TenValue);
+    localStorage.setItem("ElevenAMPlans", ElevenValue);
+    localStorage.setItem("TwelvePMPlans", TwelveValue);
+    localStorage.setItem("OnePMPlans", OneValue);
+    localStorage.setItem("TwoPMPlans", TwoValue);
+    localStorage.setItem("ThreePMPlans", ThreeValue);
+    localStorage.setItem("FourPMPlans", FourValue);
+    localStorage.setItem("FivePMPlans", FiveValue);
 }
 
-function keepInfo() {}
+function renderPlans() {
+    var NinePlans = localStorage.getItem("NineAMPlans");
+    var TenPlans = localStorage.getItem("TenAMPlans");
+    var ElevenPlans = localStorage.getItem("ElevenAMPlans");
+    var TwelvePlans = localStorage.getItem("TwelvePMPlans");
+    var OnePlans = localStorage.getItem("OnePMPlans");
+    var TwoPlans = localStorage.getItem("TwoPMPlans");
+    var ThreePlans = localStorage.getItem("ThreePMPlans");
+    var FourPlans = localStorage.getItem("FourPMPlans");
+    var FivePlans = localStorage.getItem("FivePMPlans");
 
-for (var i = 0; i < saveButton.length; i++) {
-    saveButton[i].addEventListener("click", function (event) {
+    NineAM.innerHTML = NinePlans;
+    TenAM.innerHTML = TenPlans;
+    ElevenAM.innerHTML = ElevenPlans;
+    TwelvePM.innerHTML = TwelvePlans;
+    OnePM.innerHTML = OnePlans;
+    TwoPM.innerHTML = TwoPlans;
+    ThreePM.innerHTML = ThreePlans;
+    FourPM.innerHTML = FourPlans;
+    FivePM.innerHTML = FivePlans;
+}
+
+for (const button of saveButton) {
+    button.addEventListener("click", function (event) {
         event.preventDefault();
         setPlans();
+        renderPlans();
     });
+}
+
+function TimeUpdate() {
+    console.log(moment().hour());
+    if (moment().hour() == 9) {
+        console.log("hello");
+        NineAM.classList.add("present");
+    } else if (moment().hour() > 9) {
+        NineAM.classList.add("past");
+    } else if (moment().hour() < 9) {
+        NineAM.classList.add("future");
+    }
+    if (moment().hour() == 10) {
+        console.log("hello");
+        TenAM.classList.add("present");
+    } else if (moment().hour() > 10) {
+        TenAM.classList.add("past");
+    } else if (moment().hour() < 10) {
+        TenAM.classList.add("future");
+    }
+    if (moment().hour() == 11) {
+        console.log("hello");
+        ElevenAM.classList.add("present");
+    } else if (moment().hour() > 11) {
+        ElevenAM.classList.add("past");
+    } else if (moment().hour() < 11) {
+        ElevenAM.classList.add("future");
+    }
+    if (moment().hour() == 12) {
+        console.log("hello");
+        TwelvePM.classList.add("present");
+    } else if (moment().hour() > 12) {
+        TwelvePM.classList.add("past");
+    } else if (moment().hour() < 12) {
+        TwelvePM.classList.add("future");
+    }
+
+    if (moment().hour() == 13) {
+        console.log("hello");
+        OnePM.classList.add("present");
+    } else if (moment().hour() > 13) {
+        OnePM.classList.add("past");
+    } else if (moment().hour() < 13) {
+        OnePM.classList.add("future");
+    }
+    if (moment().hour() == 14) {
+        console.log("hello");
+        TwoPM.classList.add("present");
+    } else if (moment().hour() > 14) {
+        TwoPM.classList.add("past");
+    } else if (moment().hour() < 14) {
+        TwoPM.classList.add("future");
+    }
+    if (moment().hour() == 15) {
+        console.log("hello");
+        ThreePM.classList.add("present");
+    } else if (moment().hour() > 15) {
+        ThreePM.classList.add("past");
+    } else if (moment().hour() < 15) {
+        ThreePM.classList.add("future");
+    }
+    if (moment().hour() == 16) {
+        console.log("hello");
+        FourPM.classList.add("present");
+    } else if (moment().hour() > 16) {
+        FourPM.classList.add("past");
+    } else if (moment().hour() < 16) {
+        FourPM.classList.add("future");
+    }
+    if (moment().hour() == 17) {
+        console.log("hello");
+        FivePM.classList.add("present");
+    } else if (moment().hour() > 17) {
+        FivePM.classList.add("past");
+    } else if (moment().hour() < 17) {
+        FivePM.classList.add("future");
+    }
 }
 
 // const times = [
@@ -33,7 +156,7 @@ for (var i = 0; i < saveButton.length; i++) {
 function displayTime() {
     var rightNow = moment().format("dddd, MMM DD, YYYY");
     currentDay.text(rightNow);
-    timeFormat();
+    // timeFormat();
 }
 
 // function timeFormat() {
@@ -53,8 +176,9 @@ function displayTime() {
 //     }
 // }
 
-function timeFormat() {
-    console.log("hello");
-}
-
+// function timeFormat() {
+//     console.log("hello");
+// }
+TimeUpdate();
+renderPlans();
 displayTime();
